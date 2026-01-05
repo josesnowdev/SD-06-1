0b0f45d2480801da1e489541d18819d4097098a7
josesnowdev <j.evelionieves@gmail.com>
2026-01-05 14:43:03 -0600

    Se agregan los cambios a las tareas de 1 a 4

--- task3.js
+++ task3.js
@@ -4,9 +4,12 @@ function Mail(subj, msg) {
     this.subject = subj
     this.message = msg
     
+    this.printMail = function() {
+      console.log(`${this.subject}: ${this.message}`);
+    };
   }
   
-  const newMail = new Mail(  )
+  const newMail = new Mail(process.argv[3], process.argv[4])
   
   // Type your code above this line!
   
--- task4.js
+++ task4.js
@@ -1,9 +1,15 @@
 // Type your code below this line!
 
+const from = process.argv[3];
+const to = process.argv[4];
 
+function Journey(start, end){
+    this.start = start;
+    this.end = end;
+}
 
 // Type your code above this line!
 
-const travel = new Journey(from, to)
+const travel = new Journey(from, to);
 
 console.log("Booking a taxi from " + travel.start + " to " + travel.end + ".")
